a1963b52fbe5aa44baf3cc11e7bb3fda5a49c881
CrownPab <gagandeep.pabla@ontariotechu.net>
2020-06-06 22:36:15 -0400

    Magic 8 Ball App
    This app will randomize the answer in the 8 ball when the user taps on the 8 ball.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,53 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: ballPage(),
       ),
     );
+
+
+class ballPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('AskMeAnything')),
+        backgroundColor: Colors.black26,
+      ),
+      body: ball(),
+      backgroundColor: Colors.blueAccent,
+    );
+  }
+}
+
+class ball extends StatefulWidget {
+  @override
+  _ballState createState() => _ballState();
+}
+
+class _ballState extends State<ball> {
+  int magicBallNumber = 1;
+
+  void rollBallNumber (){
+    setState(() {
+      magicBallNumber = Random().nextInt(5) + 1;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+
+        child: FlatButton(
+            onPressed: (){
+                rollBallNumber();
+            },
+            child: Image.asset('images/ball$magicBallNumber.png')),
+    );
+
+
+  }
+}
+
+
